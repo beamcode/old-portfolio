fe955f8663d59eda9d5b0535fb5dcce1f07cca72
Mac <mac@Macs-MacBook-Air.local>
2022-12-10 18:42:44 +0900

    t

--- src/pages/Home.page.js
+++ src/pages/Home.page.js
@@ -42,11 +42,11 @@ export default function HomePage() {
                             <div>
                                 <div className='w-full flex justify-between'>
                                     <h1 className="mb-3 text-5xl md:text-[40px] w-fit font-extrabold text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Samuel Palmer</h1>
-                                    <button onClick={() => setMode()} aria-label="Toggle Dark Mode" type="button" class="w-9 h-9 bg-gray-200 rounded-lg dark:bg-gray-600 flex items-center justify-center hover:ring-2 ring-gray-300 transition-all">
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" className="w-5 h-5 hidden dark:block">
+                                    <button onClick={() => setMode()} aria-label="Toggle Dark Mode" type="button" class="w-9 h-9 bg-gray-200 rounded-lg dark:bg-[#444444] flex items-center justify-center hover:ring-2 ring-gray-300 transition-all">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" className="w-5 h-5 dark:hidden">
                                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
                                         </svg>
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" className="w-5 h-5 dark:hidden">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" className="w-5 h-5 hidden dark:block text-gray-800 dark:text-gray-200">
                                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
                                         </svg>
                                     </button>
