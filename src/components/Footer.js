c0ad6b1021d37606f3a8954dbe0d65b280ed6c2e
Mac <mac@Macs-MacBook-Air.local>
2022-12-16 16:23:38 +0900

    .

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,10 @@
+import moment from "moment";
 import React from "react";
 
 export default function Footer() {
     return (
         <footer className="flex w-full max-w-[1000px] p-2 mb-10 rounded-xl items-center justify-center bg-[white] dark:bg-[#181A1B]">
-            <p className='text-[gray] text-sm whitespace-pre'>2023 © </p>
+            <p className='text-[gray] text-sm whitespace-pre'>{moment().format('YYYY')} © </p>
             <p className='text-[gray] text-sm'>Made with hate 👹 by Beamcode</p>
         </footer>
     )
